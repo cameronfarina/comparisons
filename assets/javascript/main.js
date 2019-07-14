e67747c0d0429d477180c395163fc084494b2048
Mackie <cameronfarina@gmail.com>
2019-07-14 11:22:30 -0400

    Created the ability to display phone specs upon clicking learn more

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,14 +3,20 @@
 
 const learnMoreButtons = document.querySelectorAll('.learn-more-phone-button');
 
-learnMoreButtons.forEach(function(learnMoreButton) {
-  learnMoreButton.addEventListener("click", event => {
-    const columnContentParent = learnMoreButton.closest('.column-content');
-
-    if (columnContentParent.classList.contains('in-learn-more')){
-      columnContentParent.classList.remove('in-learn-more');
-    } else {
-      columnContentParent.classList.add('in-learn-more');
-    }
-  });
+learnMoreButtons.forEach(learnMoreButton => {
+  learnMoreButton.addEventListener("click", () => _handleLearnMoreClick(learnMoreButton));
 });
+
+function _handleLearnMoreClick(btn) {
+  const columnContentParent = btn.closest('.column-content');
+  const learnMoreList = document.querySelector('.learn-more-list');
+
+  if (columnContentParent.classList.contains('in-learn-more')) {
+    columnContentParent.classList.remove('in-learn-more');
+    learnMoreList.classList.remove('clicked');
+  } else {
+    columnContentParent.classList.add('in-learn-more');
+    learnMoreList.classList.add('clicked');
+  }
+};
+
