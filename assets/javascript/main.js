1fe2e8af1895931989ed51db78898d66ce2b71bd
Mackie <cameronfarina@gmail.com>
2019-07-14 14:37:27 -0400

    Created modal for compare iphone models

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,22 +1,60 @@
 // Adding classes: https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
 // Get closest: https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
 
-const learnMoreButtons = document.querySelectorAll('.learn-more-phone-button');
+// const learnMoreButtons = document.querySelectorAll('.learn-more-phone-button');
 
-learnMoreButtons.forEach(learnMoreButton => {
-  learnMoreButton.addEventListener("click", () => _handleLearnMoreClick(learnMoreButton));
-});
+// learnMoreButtons.forEach(learnMoreButton => {
+//   learnMoreButton.addEventListener("click", () => _handleLearnMoreClick(learnMoreButton));
+// });
 
-function _handleLearnMoreClick(btn) {
-  const columnContentParent = btn.closest('.column-content');
-  const learnMoreList = document.querySelector('.learn-more-list');
+// function _handleLearnMoreClick(btn) {
+//   const columnContentParent = btn.closest('.column-content');
+//   const learnMoreList = document.querySelector('.learn-more-list');
 
-  if (columnContentParent.classList.contains('in-learn-more')) {
-    columnContentParent.classList.remove('in-learn-more');
-    learnMoreList.classList.remove('clicked');
-  } else {
-    columnContentParent.classList.add('in-learn-more');
-    learnMoreList.classList.add('clicked');
+//   if (columnContentParent.classList.contains('in-learn-more')) {
+//     columnContentParent.classList.remove('in-learn-more');
+//     learnMoreList.classList.remove('clicked');
+//   } else {
+//     columnContentParent.classList.add('in-learn-more');
+//     learnMoreList.classList.add('clicked');
+//   }
+// };
+
+// const learnMoreButtons = document.querySelectorAll('.learn-more-phone-button');
+
+// learnMoreButtons.forEach(learnMoreButton => {
+//   learnMoreButton.addEventListener("click", () => _handleLearnMoreClick(learnMoreButton));
+// });
+
+// Get modal element
+const modal = document.getElementById('simpleModal');
+// Get open modal button
+const modalBtn = document.getElementById('modalBtn');
+// Get close button 
+const closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+// Listen for open click
+modalBtn.addEventListener('click', openModal);
+// Listen for close click
+closeBtn.addEventListener('click', closeModal);
+// Listen for outside click
+window.addEventListener('click', clickOutside);
+
+// Function to open modal
+function openModal() {
+  modal.style.display = 'block';
+}
+
+//Function to close modal
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+//Function to close modal if outside click
+function clickOutside(e) {
+  if(e.target === modal){
+    modal.style.display = 'none';
   }
-};
+}
+
 
