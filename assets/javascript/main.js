737675b17140d13aab5ef22bb6398fc62c3cbc9b
Mackie <cameronfarina@gmail.com>
2019-09-13 09:21:42 -0400

    updated

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,6 +3,7 @@
   const body = document.querySelector("body");
   const modalBtn = document.getElementById("modalBtn");
   const closeBtn = document.getElementsByClassName("closeBtn")[0];
+  const headerTitle = document.getElementById("title");
 
   modalBtn.addEventListener("click", openModal);
   closeBtn.addEventListener("click", closeModal);
@@ -10,35 +11,39 @@
 
   function openModal() {
     modal.style.display = "block";
+    headerTitle.style.visibility = "hidden";
+    document.body.backgroundColor = 'black'
   }
 
   function closeModal() {
     modal.style.display = "none";
+    headerTitle.style.visibility = "visible";
   }
 
   function handleModalClick(e) {
     if (e.target === body) {
       modal.style.display = "none";
+      headerTitle.style.visibility = "visible";
     }
   }
 })();
 
+filterInput.addEventListener("keyup", filterSearch);
 
-  let filterInput = document.getElementById('filterInput');
+function filterSearch(evt) {
+  let filterInput = evt.target.value.toUpperCase();
+  let phoneColumnCollection = document.getElementsByClassName(
+    "learn-more-list"
+  );
 
-  filterInput.addEventListener('keyup', filterSearch);
-  function filterSearch(){
-    let filteredUserSearch = document.getElementById('filterInput').value.toUpperCase();
-    let phoneColumnCollection = document.getElementsByClassName('learn-more-list');
-    
-    for(let i = 0; i < phoneColumnCollection.length; i++) {
-      let phoneColumnUL = phoneColumnCollection[i];
-      for (let j = 0; j < phoneColumnUL.children.length; j++) {
-        let individualDescription = phoneColumnUL.children[j];
-        if (individualDescription.innerHTML.toUpperCase().indexOf(filteredUserSearch) > -1) {
-        individualDescription.style.display = '';
+  for (let i = 0; i < phoneColumnCollection.length; i++) {
+    let phoneColumnUL = phoneColumnCollection[i];
+    for (let j = 0; j < phoneColumnUL.children.length; j++) {
+      let individualDescription = phoneColumnUL.children[j];
+      if (individualDescription.innerText.toUpperCase().includes(filterInput)) {
+        individualDescription.style.display = "";
       } else {
-        individualDescription.style.display = 'none';
+        individualDescription.style.display = "none";
       }
     }
   }
